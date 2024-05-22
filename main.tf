70f5f8ebdefbbf6026662dd12d2e552480d7f0c6
Jerome A <jerome@ajformation.fr>
2024-05-22 13:59:38 +0000

    delete debug log

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ provider "proxmox" {
   # Configuration options
   pm_api_url = "https://cfai2024.ajformation.fr:8006/api2/json"
 
-  pm_log_enable = true
-  pm_log_file   = "terraform-plugin-proxmox.log"
-  pm_debug      = true
-  pm_log_levels = {
-    _default    = "debug"
-    _capturelog = ""
-	}
+#  pm_log_enable = true
+#  pm_log_file   = "terraform-plugin-proxmox.log"
+#  pm_debug      = true
+#  pm_log_levels = {
+#    _default    = "debug"
+#    _capturelog = ""
+#	}
 }
